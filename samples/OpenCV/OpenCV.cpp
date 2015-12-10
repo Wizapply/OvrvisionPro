62821cae6aa80e3e71227334c29b6df48eca46f9
kuronekodaisuki <pipo0921>
2015-12-11 02:12:37 +0900

    OpenMP化検討

--- samples/OpenCV/OpenCV.cpp
+++ samples/OpenCV/OpenCV.cpp
@@ -25,115 +25,6 @@ int callback(void *pItem, const char *extensions)
 	return 0;
 }
 
-// Estimate skin color 
-/*
-void estimateSkincolor(Mat &histgram, OvrvisionPro &camera, ROI roi)
-{
-	Mat left(roi.height, roi.width, CV_8UC4);
-	Mat right(roi.height, roi.width, CV_8UC4);
-	Mat images[2];
-	images[0].create(roi.height, roi.width, CV_8UC4);
-	images[1].create(roi.height, roi.width, CV_8UC4);
-
-	int channels[] = { 0, 1 }; // H, S
-	int histSize[] = { 256, 256 };
-	float hranges[] = { 0, 256 };
-	float sranges[] = { 0, 256 };
-	const float* ranges[] = { hranges, sranges };
-
-	while (waitKey(10) != 'a')
-	{
-		// Capture frame
-		ovrvision.Capture(Camqt::OV_CAMQT_DMSRMP);
-
-		// Retrieve frame data
-		ovrvision.GetStereoImageBGRA(left.data, right.data, roi);
-		imshow("Left", left);
-		imshow("Right", right);
-	}
-
-	// back ground
-	Mat background(256, 256, CV_32FC1);
-	background.setTo(Scalar::all(0));
-	for (int i = 0; i < 50; i++)
-	{
-		// Capture frame
-		ovrvision.Capture(Camqt::OV_CAMQT_DMSRMP);
-
-		// Retrieve frame data
-		ovrvision.GetStereoImageBGRA(left.data, right.data, roi);
-		cvtColor(left, images[0], CV_BGR2HSV_FULL);
-		cvtColor(right, images[1], CV_BGR2HSV_FULL);
-		calcHist(images, 2, channels, Mat(), background, 2, histSize, ranges, true, true);
-
-		imshow("Left", left);
-		imshow("Right", right);
-	}
-	while (waitKey(10) != 'a')
-	{
-		// Capture frame
-		ovrvision.Capture(Camqt::OV_CAMQT_DMSRMP);
-
-		// Retrieve frame data
-		ovrvision.GetStereoImageBGRA(left.data, right.data, roi);
-
-		putText(left, "Waiting...", Point(0, 50), cv::FONT_HERSHEY_PLAIN, 3, Scalar(0, 255, 255), 2);
-		putText(right, "Waiting...", Point(0, 50), cv::FONT_HERSHEY_PLAIN, 3.0, Scalar(0, 255, 255), 2);
-		imshow("Left", left);
-		imshow("Right", right);
-	}
-
-	// weave hands
-	Mat foreground(256, 256, CV_32FC1);
-	foreground.setTo(Scalar::all(0));
-	for (int i = 0; i < 50; i++)
-	{
-		// Capture frame
-		ovrvision.Capture(Camqt::OV_CAMQT_DMSRMP);
-
-		// Retrieve frame data
-		ovrvision.GetStereoImageBGRA(left.data, right.data, roi);
-		cvtColor(left, images[0], CV_BGR2HSV_FULL);
-		cvtColor(right, images[1], CV_BGR2HSV_FULL);
-		calcHist(images, 2, channels, Mat(), foreground, 2, histSize, ranges, true, true);
-
-		putText(left, "Initiating...", Point(0, 50), cv::FONT_HERSHEY_PLAIN, 3, Scalar(0, 255, 255), 2);
-		putText(right, "Initiating...", Point(0, 50), cv::FONT_HERSHEY_PLAIN, 3.0, Scalar(0, 255, 255), 2);
-		imshow("Left", left);
-		imshow("Right", right);
-	}
-	
-	// Compare histgram
-	Mat difference(256, 256, CV_32FC1);
-	difference.setTo(Scalar::all(0));
-	float maxVal = 0;
-	for (int h = 3; h < 256; h++)
-	{
-		float *b = background.ptr<float>(h);
-		float *f = foreground.ptr<float>(h);
-		float *d = difference.ptr<float>(h);
-		for (int s = 0; s < 256; s++)
-		{
-			float diff = f[s] - b[s];
-			if (0 < diff)
-			{
-				d[s] = diff;
-				if (maxVal < diff)
-					maxVal = diff;
-			}
-		}
-	}
-	Mat diff(256, 256, CV_32FC1);
-	GaussianBlur(difference, diff, Size(3, 3), 8, 6);
-	//normalize(background, histgram, 0, 255, NORM_MINMAX, histgram.type());
-	//imshow("background", histgram);
-	//normalize(foreground, histgram, 0, 255, NORM_MINMAX, histgram.type());
-	//imshow("foreground", histgram);
-	normalize(diff, histgram, 0, 255, NORM_MINMAX, histgram.type());
-	imshow("histgram", histgram);
-	imwrite("histgram.png", histgram);
-}
-*/
 
 enum FILTER {
 	GAUSSIAN,
@@ -151,11 +42,15 @@ int main(int argc, char* argv[])
 		int width = ovrvision->GetCamWidth() / 2;
 		int height = ovrvision->GetCamHeight() / 2;
 		ROI roi = { 0, 0, width, height };
+		Camqt mode = Camqt::OV_CAMQT_DMSRMP;
+		//bool show = true;
+		bool useHistgram = false;
 
 		Mat images[2];
 		Mat bilevel[2];
 		Mat hsv[2], HSV[2];
 		Mat results[2];
+		Mat histgram(180, 256, CV_8UC1);
 
 		results[0].create(height, width, CV_8UC4);
 		results[1].create(height, width, CV_8UC4);
@@ -168,40 +63,8 @@ int main(int argc, char* argv[])
 		bilevel[0].create(height, width, CV_8UC1);
 		bilevel[1].create(height, width, CV_8UC1);
 
-
-		Mat histgram(180, 256, CV_8UC1);
-
-
 		//Sync
 		ovrvision->SetCameraSyncMode(true);
-
-		Camqt mode = Camqt::OV_CAMQT_DMSRMP;
-		bool show = true;
-		bool useHistgram = false;
-
-		// Read histgram
-		//histgram = imread("histgram.bmp");
-		if (histgram.empty())
-		{
-			//estimateSkincolor(histgram, ovrvision, roi);
-		}
-		else
-		{
-			//useHistgram = true;
-			//imshow("histgram", histgram);
-			/*
-			for (int y = 0; y < 256; y += 4)
-			{
-				printf("H:%03d, ", y);
-				uchar *row = histgram.ptr<uchar>(y);
-				for (int x = 0; x < 256; x += 4)
-				{
-					printf("%d, ", row[x]);
-				}
-				puts("");
-			}
-			*/
-		}
 
 		//_h_low = 13;
 		//_h_high = 21;
@@ -211,117 +74,104 @@ int main(int argc, char* argv[])
 
 		for (bool loop = true; loop;)
 		{
-			if (show)
+			//if (show)
+			//{
+			// Capture frame
+			ovrvision->Capture(mode);
+
+			// Retrieve frame data
+			ovrvision->Read(images[0].data, images[1].data);				
+			ovrvision->GetStereoImageHSV(hsv[0].data, hsv[1].data);
+
+			// ここでOpenCVでの加工など
+			if (0 < ksize)
 			{
-				// Capture frame
-				ovrvision->Capture(mode);
-
-				// Retrieve frame data
-				ovrvision->Read(images[0].data, images[1].data);				
-				ovrvision->GetStereoImageHSV(hsv[0].data, hsv[1].data);
-
-				// ここでOpenCVでの加工など
-				if (0 < ksize)
+				switch (filter)
 				{
-					switch (filter)
-					{
-					case GAUSSIAN:
-						GaussianBlur(hsv[0], HSV[0], Size(ksize, ksize), 0);
-						GaussianBlur(hsv[1], HSV[1], Size(ksize, ksize), 0);
-						break;
+				case GAUSSIAN:
+					GaussianBlur(hsv[0], HSV[0], Size(ksize, ksize), 0);
+					GaussianBlur(hsv[1], HSV[1], Size(ksize, ksize), 0);
+					break;
 
-					case MEDIAN:
-						medianBlur(hsv[0], HSV[0], ksize);
-						medianBlur(hsv[1], HSV[1], ksize);
-						break;
+				case MEDIAN:
+					medianBlur(hsv[0], HSV[0], ksize);
+					medianBlur(hsv[1], HSV[1], ksize);
+					break;
 
-					default:
-						hsv[0].copyTo(HSV[0]);
-						hsv[1].copyTo(HSV[1]);
-						break;
-					}
+				default:
+					hsv[0].copyTo(HSV[0]);
+					hsv[1].copyTo(HSV[1]);
+					break;
+				}
 					
 #					pragma omp parallel for
-					for (int i = 0; i < 2; i++)
-					{
-						results[i].setTo(Scalar(0, 0, 0, 255));
-						bilevel[i].setTo(Scalar::all(0));
+				for (int i = 0; i < 2; i++)
+				{
+					results[i].setTo(Scalar(0, 0, 0, 255));
+					bilevel[i].setTo(Scalar::all(0));
 
-						for (int y = 0; y < height; y++)
+					for (int y = 0; y < height; y++)
+					{
+						Vec4b *l = HSV[i].ptr<Vec4b>(y);
+						Vec4b *Lpixel = results[i].ptr<Vec4b>(y);
+						uchar *b_l = bilevel[i].ptr<uchar>(y);
+						for (int x = 0; x < width; x++)
 						{
-							Vec4b *l = HSV[i].ptr<Vec4b>(y);
-							Vec4b *Lpixel = results[i].ptr<Vec4b>(y);
-							uchar *b_l = bilevel[i].ptr<uchar>(y);
-							for (int x = 0; x < width; x++)
+							uchar h = l[x][0];
+							uchar s = l[x][1];
+							if (useHistgram)
 							{
-								uchar h = l[x][0];
-								uchar s = l[x][1];
-								if (useHistgram)
+								if (1 < histgram.at<uchar>(h, s))
 								{
-									if (1 < histgram.at<uchar>(h, s))
-									{
-										Lpixel[x] = images[i].at<Vec4b>(y, x);
-										b_l[x] = 255;
-									}
+									Lpixel[x] = images[i].at<Vec4b>(y, x);
+									b_l[x] = 255;
 								}
-								else
+							}
+							else
+							{
+								if (10 <= h && h <= 26 && 55 < s && s < 150)
 								{
-									if (10 <= h && h <= 26 && 55 < s && s < 150)
-									{
-										Lpixel[x] = images[i].at<Vec4b>(y, x);
-										b_l[x] = 255;
-									}
+									Lpixel[x] = images[i].at<Vec4b>(y, x);
+									b_l[x] = 255;
 								}
 							}
 						}
 					}
-					// ここまでGPU（OpenCL）で
+				}
+				// ここまでGPU（OpenCL）で
 
-					// TODO: ここはOpenMPで左右を並行処理する
-					// CPU側（OpenCV）
+				// TODO: ここはOpenMPで左右を並行処理する
+				// CPU側（OpenCV）
 #					pragma omp parallel for
-					for (int eyes = 0; eyes < 2; eyes++)
-					{
-						std::vector<std::vector<Point>> contours;
-						std::vector<Vec4i> hierarchy;
-
-						findContours(bilevel[eyes], contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
-						for (uint i = 0; i < contours.size(); i++)
-						{
-							if (200 < contours[i].size())
-								drawContours(results[eyes], contours, i, Scalar(255, 255, 255), 1, 8);
-						}
-					}
-
-					// ここまでOpenCVで処理してGPUに戻す
-
-					// Show frame data
-					imshow("bilevel(L)", bilevel[0]);
-					imshow("bilevel(R)", bilevel[1]);
-					imshow("Left", images[0]);
-					imshow("Right", images[1]);
-					imshow("L", results[0]);
-					imshow("R", results[1]);
-				}
-				else
+				for (int eyes = 0; eyes < 2; eyes++)
 				{
-					imshow("Left", images[0]);
-					imshow("Right", images[1]);
+					std::vector<std::vector<Point>> contours;
+					std::vector<Vec4i> hierarchy;
+
+					findContours(bilevel[eyes], contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
+					for (uint i = 0; i < contours.size(); i++)
+					{
+						if (200 < contours[i].size())
+							drawContours(results[eyes], contours, i, Scalar(255, 255, 255), 1, 8);
+					}
 				}
+
+				// ここまでOpenCVで処理してGPUに戻す
+
+				// Show frame data
+				//imshow("bilevel(L)", bilevel[0]);
+				//imshow("bilevel(R)", bilevel[1]);
+				imshow("Left", images[0]);
+				imshow("Right", images[1]);
+				imshow("L", results[0]);
+				imshow("R", results[1]);
 			}
 			else
 			{
-				ovrvision->Capture(mode);
-				ovrvision->Read(images[0].data, images[1].data);
-				ovrvision->SkinRegion(bilevel[0].data, bilevel[1].data);
-				//ovrvision->GrayscaleHalf(bilevel_l.data, bilevel_r.data);
-				ovrvision->ColorHistgram(histgram.data);
-				imshow("histgram", histgram);
 				imshow("Left", images[0]);
 				imshow("Right", images[1]);
-				imshow("bilevel(L)", bilevel[0]);
-				imshow("bilevel(R)", bilevel[1]);
 			}
+			//}
 
 			switch (waitKey(1))
 			{
@@ -341,9 +191,9 @@ int main(int argc, char* argv[])
 				useHistgram = !useHistgram;
 				break;
 
-			case 's':
-				show = !show;
-				break;
+			//case 's':
+			//	show = !show;
+			//	break;
 
 			case 'g':
 				filter = GAUSSIAN;
@@ -379,7 +229,7 @@ int main(int argc, char* argv[])
 				break;
 
 			case ' ':
-				if (show)
+				//if (show)
 				{
 					imwrite("left.png", images[0]);
 					imwrite("right.png", images[1]);
@@ -393,14 +243,13 @@ int main(int argc, char* argv[])
 					//imwrite("blur_r.png", blur2);
 					//imwrite("histgram.png", histgram);
 				}
-				else
-				{
-					imwrite("LEFT.tiff", images[0]);
-					imwrite("RIGHT.tiff", images[1]);
-					imwrite("bilevel_l.png", bilevel[0]);
-					imwrite("bilevel_r.png", bilevel[1]);
-
-				}
+				//else
+				//{
+				//	imwrite("LEFT.tiff", images[0]);
+				//	imwrite("RIGHT.tiff", images[1]);
+				//	imwrite("bilevel_l.png", bilevel[0]);
+				//	imwrite("bilevel_r.png", bilevel[1]);
+				//}
 				break;
 
 			case 'e':
