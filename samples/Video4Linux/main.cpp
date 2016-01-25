35e3f0bb9a3e1deb7d10f6bfccf9f36e2312235e
Hiroshi Matsuoka <pipomaomy2005@gmail.com>
2016-01-25 13:54:08 +0000

    Add Video4Linux query program

---
+++ samples/Video4Linux/main.cpp
@@ -0,0 +1,20 @@
+#include "ovrvision_v4l.h"
+
+using namespace OVR;
+
+int main(int argc, char *argv[])
+{
+	OvrvisionVideo4Linux v4l;
+	v4l.OpenDevice(0, 0, 0, 0);
+	v4l.QueryCapability();
+}
+
+/* Result
+Driver name     : uvcvideo
+Driver Version  : 3.10.40
+Device name     : OvrvisionPro
+Bus information : usb-tegra-ehci.0-1
+Capabilities    : 84000001h
+V4L2_CAP_VIDEO_CAPTURE (Video Capture)
+V4L2_CAP_STREAMING (Streaming I/O method)
+*/
