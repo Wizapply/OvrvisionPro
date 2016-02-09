94a424fb9358a49efb11d262cd881ee7037c8581
Hiroshi Matsuoka <pipomaomy2005@gmail.com>
2016-02-09 18:01:07 +0000

    Add Jetson TK1 project

---
+++ build/linux/JetsonTK1/Release/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../main.cpp \
+../ovrvision_v4l.cpp 
+
+OBJS += \
+./main.o \
+./ovrvision_v4l.o 
+
+CPP_DEPS += \
+./main.d \
+./ovrvision_v4l.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
