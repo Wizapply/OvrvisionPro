7345256aa9a6a08500866cea0f2ede82e8e5cdbb
kuronekodaisuki <pipomaomy2005@gmail.com>
2016-02-12 03:47:31 +0900

    modify main.cpp

--- build/linux/CUDA/main.cpp
+++ build/linux/CUDA/main.cpp
@@ -1,1 +1,54 @@
-../../../samples/Video4Linux/main.cpp
+#include <opencv2/opencv.hpp>
+#include "ovrvision_v4l.h"
+#include "OvrvisionProCUDA.h"
+
+using namespace OVR;
+using namespace cv;
+
+#define WIDTH 640
+#define HEIGHT	480
+
+int main(int argc, char *argv[])
+{
+	Mat image(HEIGHT, WIDTH, CV_16UC1);
+	Mat left(HEIGHT, WIDTH, CV_8UC3);
+	Mat right(HEIGHT, WIDTH, CV_8UC3);
+	OvrvisionProCUDA cuda(WIDTH, HEIGHT);
+
+	OvrvisionVideo4Linux v4l;
+	v4l.OpenDevice(0, WIDTH, HEIGHT, 0);
+	v4l.QueryCapability();
+	v4l.EnumFormats();
+	v4l.StartTransfer();
+	for (bool loop = true; loop;)
+	{
+		if (0 == v4l.GetBayer16Image(image.data))
+		{
+			cuda.Demosaic(image, left, right);
+			imshow("Bayer", image);
+			imshow("Left", left);
+			imshow("Right", right);
+		}
+		switch (waitKey(10))
+		{case 'q':
+			loop = false;
+			break;
+		case ' ':
+			imwrite("frame.png", image);
+			break;
+		}
+	}
+	v4l.StopTransfer();
+	v4l.DeleteDevice();
+}
+
+/* Result
+Driver name     : uvcvideo
+Driver Version  : 3.10.40
+Device name     : OvrvisionPro
+Bus information : usb-tegra-ehci.0-1
+Capabilities    : 84000001h
+V4L2_CAP_VIDEO_CAPTURE (Video Capture)
+V4L2_CAP_STREAMING (Streaming I/O method)
+*/
+\
